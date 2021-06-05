d18b2ee59812dd43d4dab22ae2f732d26aeb2e34
Santiagowebservices <83625508+Santiagowebservices@users.noreply.github.com>
2021-06-05 20:47:24 -0300

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+/*
+Posea un script en javascript que declare:
+ 
+
+una variable llamada primera con un valor numérico = 40
+una variable llamada segunda con un valor numérico = 16
+una variable llamada sumatotal con un valor numérico de la suma de las dos variables anteriores.
+ 
+
+y una salida por pantalla en HTML que muestre un mensaje y el valor de sumatotal:
+*/
+
+var primera = 40;
+var segunda = 16;
+var sumatotal = primera + segunda;
+document.write("<h1>El resultado de la suma de las dos variables es: <br>" + "<br>" + (sumatotal) + "</h1>");
